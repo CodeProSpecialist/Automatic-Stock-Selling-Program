983e8b8894bd03b02fdd76d993052411e3ba875b
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-04-21 00:08:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# making sure python3.11 can install packages by renaming EXTERNALLY-MANAGED to EXTERNALLY-MANAGED.old
-sudo mv /usr/lib/python3.11/EXTERNALLY-MANAGED /usr/lib/python3.11/EXTERNALLY-MANAGED.old 
 
 # Install required Python packages
 pip3 install alpaca-trade-api pytz
